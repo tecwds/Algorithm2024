e227ba472e4a14b205bd315177e17db830a08ce7
tecwds <tecwds@163.com>
2024-09-19 08:21:54 +0800

    add：换钞票

---
+++ AlgContest1494/C/PCv2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+// 10
+int a;
+
+// 5
+int b;
+
+// 2
+int c;
+
+// 1
+int d;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    for (a = 1; a < n; a++) {
+        for (b = 1; b < n; b++) {
+            for (c = 1; c < n; c++) {
+                d = n - a - b - c;
+
+                if (d < 0) {
+                    d = 0;
+                }
+
+                if (a * 10 + b * 5 + c * 2 + d == 100 &&
+                    a + b + c + d == n
+                ) {
+                    cout
+                     << a << " "
+                     << b << " "
+                     << c << " "
+                     << d << endl;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
