d3f1ae42884e7ee4f5ab0e2ab977156c85f0456a
tecwds <tecwds@163.com>
2024-09-19 08:20:24 +0800

    add：ASCII码

---
+++ AlgContest1486/A/PAv1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int t;
+int ascll[1011];
+// int input;
+string str;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> t;
+    int i = 1;
+    while (getline(cin, str)) {
+        
+    }
+
+    return 0;
+}
