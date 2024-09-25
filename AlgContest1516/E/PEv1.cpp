b5d657ada8fac5cc4404924ad85836e703e71624
tecwds <tecwds@163.com>
2024-09-25 17:47:04 +0800

    add: 不容易系列之一

---
+++ AlgContest1516/E/PEv1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// f1 = 0
+// f2 = 1
+// f3 = 2
+// 
+
+long long cnn(int n, int i) {
+    for (int j = 0; j <)
+}
+
+
+int main() {
+
+    int n;
+
+    while (~scanf("%d", &n)) {
+        long long f1 = 0;
+        long long f2 = 1;
+        long long fn = 0;
+
+        if (n == 1) {
+            cout << f1 << endl;
+            continue;
+        } else if (n == 2) {
+            cout << f2 << endl;
+            continue;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            fn = 
+        }
+    }
+
+    return 0;
+}
