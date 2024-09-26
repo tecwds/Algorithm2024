e3372bfdab8723da28fed325218ad94c1f1c2538
tecwds <tecwds@163.com>
2024-09-26 09:24:29 +0800

    mod: 油桶问题 - 递归版

--- AlgContest1536/F/PFv1.cpp
+++ AlgContest1536/F/PFv1.cpp
@@ -1,37 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int func(int oil[], int n, int m, int index, int sum){
-    if(index == n){
-        return sum;
-    }
-    int t = func(oil, n, m, index + 1, sum);
-    if(t > m){
-        return -1; // 如果已经超过m了，就返回一个不可能的值
-    }
-    int t2 = func(oil, n, m, index + 1, sum + oil[index]);
-    if(t2 == m){
-        return m; // 找到符合条件的组合
-    }else{
-        return -1; // 没找到，继续找下一个
-    }
+int a[1001];
+
+bool check(int n, int m) {
+    if (n == 0) return false;
+    if (a[n] == m) return true;
+    if (check(n - 1, m - a[n])) return true;
+    if (check(n - 1, m)) return true;
+    return false;
 }
 
 int main() {
-    int n, m;
+    int n;
+    int m;
+
     cin >> n >> m;
-    int oil[105];
-    for(int j = 0; j < n; j++){
-        cin >> oil[j];
+
+    for (int j = 1; j <= n; j++) {
+        cin >> a[j];
     }
 
-    int result = func(oil, n, m, 0, 0);
-
-    if(result >= m){
-        cout << "yes" << endl;
-    }else{
-        cout << "no" << endl;
-    }
+    if (check(n, m)) cout << "yes" << endl;
+    else cout << "no" << endl;
 
     return 0;
 }
