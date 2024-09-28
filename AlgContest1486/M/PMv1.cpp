5f19a517bc17c98c0caf1b6cbbca8d568aab424d
tecwds <tecwds@163.com>
2024-09-29 01:11:35 +0800

    finish: 酱香型牛奶

--- AlgContest1486/M/PMv1.cpp
+++ AlgContest1486/M/PMv1.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
-#include <map>
+
 using namespace std;
 
-m
+int s1;
+int s2;
+int a[4];
+int b[4];
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int i;
+    int j;
+
+    for (i = 0; i < 3; i++, cin >> a[i] >> b[i]) {
+    }
+    for (i = 1; i <= 100; ++i) {
+        s1 = (i - 1) % 3 + 1;
+
+        if (s1 == 3) { s2 = 1; } else { s2 = s1 + 1; }
+
+        int min = std::min(a[s2] - b[s2], b[s1]);
+
+        b[s1] -= min;
+        b[s2] += min;
+    }
+
+    // for (const auto i_ : b) cout << i_ << endl;
+    for (int i_ = 1; i_ < 4; ++ i_) cout << b[i_] << endl;
 
     return 0;
 }
